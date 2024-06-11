048e44aeba6116bd26c3b576e2a5d4d8aeea6c6a
ImJcm <wjsckdals0909@naver.com>
2024-06-12 04:47:17 +0900

    init setting

---
+++ src/main/java/com/imjcm/oauth2andloginpractice/OAuth2AndLoginPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.imjcm.oauth2andloginpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2AndLoginPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2AndLoginPracticeApplication.class, args);
+    }
+
+}
