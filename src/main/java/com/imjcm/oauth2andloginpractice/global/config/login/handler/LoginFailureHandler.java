6bd83047db6b1581005ee9a72012a6b7595cc829
ImJcm <wjsckdals0909@naver.com>
2024-07-03 01:03:10 +0900

    LoginFailureHandler - onAuthenticationFailure  Override

--- src/main/java/com/imjcm/oauth2andloginpractice/global/config/login/handler/LoginFailureHandler.java
+++ src/main/java/com/imjcm/oauth2andloginpractice/global/config/login/handler/LoginFailureHandler.java
@@ -2,11 +2,32 @@ package com.imjcm.oauth2andloginpractice.global.config.login.handler;
 
 import com.imjcm.oauth2andloginpractice.domain.member.MemberRepository;
 import com.imjcm.oauth2andloginpractice.global.config.jwt.service.JwtService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
+import java.io.IOException;
+
+@Slf4j(topic = "로그인 인증 실패")
 @RequiredArgsConstructor
 public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
     private final JwtService jwtService;
     private final MemberRepository memberRepository;
+
+    /**
+     * UsernamePasswordAuthenticationFilter에서 인증을 마친 후, 인증이 실패할 경우 로직을 처리하는 onAuthenticationFailure 메서드룰 수행한다.
+     * @param request
+     * @param response
+     * @param exception
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        super.onAuthenticationFailure(request, response, exception);
+    }
 }
