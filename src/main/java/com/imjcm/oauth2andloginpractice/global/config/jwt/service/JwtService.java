042af2fd033fac363d7452381e11c3ac74d81d8d
ImJcm <wjsckdals0909@naver.com>
2024-07-03 01:03:29 +0900

    JWTService - @Slf4j 추가

---
+++ src/main/java/com/imjcm/oauth2andloginpractice/global/config/jwt/service/JwtService.java
@@ -0,0 +1,10 @@
+package com.imjcm.oauth2andloginpractice.global.config.jwt.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j(topic = "JWT 토큰 설정 및 유틸리티")
+@Service
+public class JwtService {
+
+}
