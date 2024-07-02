7a90269c53d5a7e3673598622000b722572867f0
ImJcm <wjsckdals0909@naver.com>
2024-07-03 01:02:51 +0900

    LoginSuccessHandler - onAuthenticationSuccess Override

--- src/main/java/com/imjcm/oauth2andloginpractice/global/config/login/handler/LoginSuccessHandler.java
+++ src/main/java/com/imjcm/oauth2andloginpractice/global/config/login/handler/LoginSuccessHandler.java
@@ -2,11 +2,32 @@ package com.imjcm.oauth2andloginpractice.global.config.login.handler;
 
 import com.imjcm.oauth2andloginpractice.domain.member.MemberRepository;
 import com.imjcm.oauth2andloginpractice.global.config.jwt.service.JwtService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
+import java.io.IOException;
+
+@Slf4j(topic = "로그인 인증 성공")
 @RequiredArgsConstructor
 public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private final JwtService jwtService;
     private final MemberRepository memberRepository;
+
+    /**
+     * UsernamePasswordAuthenticationFilter로 부터 인증을 마친 후, 인증이 성공할 경우 로직을 처리하는 onAuthenticationSuccess 메서드 수행한다.
+     * @param request
+     * @param response
+     * @param authentication
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        super.onAuthenticationSuccess(request, response, authentication);
+    }
 }
