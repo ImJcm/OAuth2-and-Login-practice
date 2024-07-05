8a5e00e9cabf5aa72065ac73690aba3bb19e3119
ImJcm <wjsckdals0909@naver.com>
2024-07-06 03:52:37 +0900

    SignupRequestDto - @NoArgConstructor, @AllArgConstructor 추가

--- src/main/java/com/imjcm/oauth2andloginpractice/domain/member/dto/request/SignupRequestDto.java
+++ src/main/java/com/imjcm/oauth2andloginpractice/domain/member/dto/request/SignupRequestDto.java
@@ -1,8 +1,13 @@
 package com.imjcm.oauth2andloginpractice.domain.member.dto.request;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class SignupRequestDto {
     private String email;
     private String nickname;
