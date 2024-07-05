11d0f9def8b2057f6acafc9095923c84172bbb71
ImJcm <wjsckdals0909@naver.com>
2024-07-06 00:18:53 +0900

    LoginSuccessHandler - CustomJsonUsernamePasswordAuthenticationFilter에서 인증 성공 시, 수행하는 onAuthenticationSuccess 메서드 추가

--- src/main/java/com/imjcm/oauth2andloginpractice/global/config/login/handler/LoginSuccessHandler.java
+++ src/main/java/com/imjcm/oauth2andloginpractice/global/config/login/handler/LoginSuccessHandler.java
@@ -1,6 +1,8 @@
 package com.imjcm.oauth2andloginpractice.global.config.login.handler;
 
+import com.imjcm.oauth2andloginpractice.domain.member.Member;
 import com.imjcm.oauth2andloginpractice.domain.member.MemberRepository;
+import com.imjcm.oauth2andloginpractice.global.common.Role;
 import com.imjcm.oauth2andloginpractice.global.config.jwt.service.JwtService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,6 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 import java.io.IOException;
@@ -20,6 +23,7 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     /**
      * UsernamePasswordAuthenticationFilter로 부터 인증을 마친 후, 인증이 성공할 경우 로직을 처리하는 onAuthenticationSuccess 메서드 수행한다.
+     * AccessToken을 생성한 후, Header를 통해 token을 전달한다.
      * @param request
      * @param response
      * @param authentication
@@ -28,6 +32,32 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
      */
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        super.onAuthenticationSuccess(request, response, authentication);
+        String email = extractEmail(authentication);
+        Role role = extractRole(authentication);
+
+        String token = jwtService.createAccessToken(email, role);
+        jwtService.sendAccessToken(response, token);
+    }
+
+    /**
+     * Authentication 객체의 Principal에 저장된 UserDetails 객체에서 email을 추출
+     * Member는 UserDetails를 상속하기 때문에 Member 타입으로 받는다.
+     * @param authentication
+     * @return
+     */
+    private String extractEmail(Authentication authentication) {
+        Member userDetails = (Member) authentication.getDetails();
+        return userDetails.getEmail();
+    }
+
+    /**
+     * Authentication 객체의 Principal에 저장된 UserDetails 객체에서 role을 추출
+     * Member는 UserDetails를 상속하기 때문에 Member 타입을 받는다.
+     * @param authentication
+     * @return
+     */
+    private Role extractRole(Authentication authentication) {
+        Member userDetails = (Member) authentication.getAuthorities();
+        return userDetails.getRole();
     }
 }
