26a425624077f535bbe9587176555c9abab00416
ImJcm <wjsckdals0909@naver.com>
2024-07-06 02:39:47 +0900

    CustomJsonUsernamePasswordAuthenticationFilter - attemptAuthentication 내부 과정 설명 추가

--- src/main/java/com/imjcm/oauth2andloginpractice/global/config/login/filter/CustomJsonUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/imjcm/oauth2andloginpractice/global/config/login/filter/CustomJsonUsernamePasswordAuthenticationFilter.java
@@ -23,6 +23,10 @@ public class CustomJsonUsernamePasswordAuthenticationFilter extends UsernamePass
     /**
      * 클라이언트로부터 입력으로 넘어오는 JSON 형태의 email(username), password 형태의 데이터를 LoginRequestDto 형태로 받고,
      * UsernamePasswordAuthenticationToken을 만들어 AuthenticationManager를 통해 사용자인지 여부를 검증 및 인증을 수행한다.
+     *  - UsernamePasswordAuthenticationToken(Principal, Credential, Authorities) - Principal = email, Credential = password
+     *  - getAuthenticationManager().authenticate()를 수행 후, Authentication인 UsernamePasswordAuthenticationToken의 구성이 변경된다.
+     *      - Principal = username에 해당하는 UserDetails가 저장, Credential : 인자로 넘어온 Password, Authority : pricipal에 담긴 UserDetails의 getAuthorities가 담긴다.
+     *      -> 확인한 부분 : AbstractUserDetailsAuthenticationProvider.class - authenticate()에서 위 과정이 수행됨
      * 인증 성공 시, 실제 DB에 존재하는 회원의 정보가 담긴 UserDetails를 상속한 클래스를 포함하는 Authentication을 반환한다.
      *  - LoginSuccessHandler
      * 인증 실패 시, 인증 실패 핸들러 수행 또는 (파라미터 인자 매핑, etc) 예외 발생
