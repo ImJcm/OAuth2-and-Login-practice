ad70bebcc42a167eb5d39213ce39d5e6d95f6b2d
ImJcm <wjsckdals0909@naver.com>
2024-07-06 00:16:35 +0900

    LoginRequestDto - @NoArgContstructor, @AllArgsConstructor 추가 (테스트 시 필요 용도 & 역직렬화 DTO 시, @Getter + @NoArgsConstructor 필요)

--- src/main/java/com/imjcm/oauth2andloginpractice/domain/member/dto/request/LoginRequestDto.java
+++ src/main/java/com/imjcm/oauth2andloginpractice/domain/member/dto/request/LoginRequestDto.java
@@ -1,9 +1,13 @@
 package com.imjcm.oauth2andloginpractice.domain.member.dto.request;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class LoginRequestDto {
     private String email;
     private String password;
