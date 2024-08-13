fcfb3df7b54c98b8712c6e895db1bb238c30d7c1
ImJcm <wjsckdals0909@naver.com>
2024-08-13 23:16:23 +0900

    JwtServiceTest - RefreshToken 관련 테스트 추가

--- src/test/java/com/imjcm/oauth2andloginpractice/jwt/Mockito/JwtServiceTest.java
+++ src/test/java/com/imjcm/oauth2andloginpractice/jwt/Mockito/JwtServiceTest.java
@@ -12,24 +12,40 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.w3c.dom.ls.LSOutput;
 
 import javax.crypto.SecretKey;
 import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class JwtServiceTest {
     @InjectMocks
     private JwtService jwtService;
 
+    @Mock
+    RedisTemplate<String, String> redisTemplate;
+
+    @Mock
+    private ValueOperations<String, String> valueOperations;
+
     //private String jwt_token;
     private SecretKey key;
     private String secretKey;
     private Long accessTokenExpirationsPeriod;
+    private Long refreshTokenExpirationsPeriod;
     private String accessTokenHeader;
+    private String refreshTokenHeader;
     public String BEARER_PREFIX;
     public String EMAIL_CLAIMS;
 
@@ -38,14 +54,18 @@ public class JwtServiceTest {
         // Mocking한 jwtService의 @Value로 설정된 변수에 값을 전달
         ReflectionTestUtils.setField(jwtService, "secretKey","amNtLW9hdXRoMi9jdXN0b21Mb2dpbkZpbHRlciBQcm9qZWN0");
         ReflectionTestUtils.setField(jwtService, "accessTokenHeader","Authorization");
+        ReflectionTestUtils.setField(jwtService, "refreshTokenHeader", "Refresh Authorization");
         ReflectionTestUtils.setField(jwtService, "accessTokenExpirationsPeriod",1800000L);
+        ReflectionTestUtils.setField(jwtService, "refreshTokenExpirationsPeriod",3600000L);
         ReflectionTestUtils.setField(jwtService, "key", Keys.hmacShaKeyFor(Decoders.BASE64.decode("amNtLW9hdXRoMi9jdXN0b21Mb2dpbkZpbHRlciBQcm9qZWN0")));
 
         // hard coding
         secretKey = "amNtLW9hdXRoMi9jdXN0b21Mb2dpbkZpbHRlciBQcm9qZWN0";
         key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
         accessTokenExpirationsPeriod = 1800000L;
+        refreshTokenExpirationsPeriod = 3600000L;
         accessTokenHeader = "Authorization";
+        refreshTokenHeader = "Refresh Authorization";
         BEARER_PREFIX = "Bearer ";
         EMAIL_CLAIMS = "email";
 
@@ -77,13 +97,53 @@ public class JwtServiceTest {
         Assertions.assertThat(email).isEqualTo(emailFromAccessToken);
     }
 
+    @DisplayName("createRefreshToken : refreshToken 생성 성공")
+    @Test
+    public void createRefreshTokenSuccess() throws Exception{
+        // given
+        String email = "testEmail";
+
+        // when
+        String token = jwtService.createRefreshToken(email);
+
+        String rjwt = token.split(" ")[1];
+
+        //String emailFromRefreshToken = String.valueOf(jwtService.extractEmailFromToken(rjwt));
+        String emailFromRefreshToken = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(rjwt)
+                .getPayload()
+                .get(EMAIL_CLAIMS, String.class);
+
+        // then
+        Assertions.assertThat(token).startsWith("Bearer ");
+        Assertions.assertThat(email).isEqualTo(emailFromRefreshToken);
+    }
+
+    @DisplayName("reIssuedRefreshToken : refreshToken 재발급 성공")
+    @Test
+    public void reIssuedRefreshTokenSuccess() throws Exception {
+        // given
+        String email = "test Email";
+
+        // when
+        when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+        doNothing().when(valueOperations).set(anyString(), anyString());
+
+        String result = jwtService.reIssuedRefreshToken(email);
+
+        // then
+        verify(valueOperations, times(1)).set(anyString(), anyString());
+        Assertions.assertThat(result).startsWith(BEARER_PREFIX);
+    }
+
     @DisplayName("sendAccessToken : AccessToken send 성공")
     @Test
     public void sendAccessTokenSuccess() throws Exception {
         // given
         HttpServletResponse response = new MockHttpServletResponse();
         String token = "Test Token";
-        String accessTokenHeader = "Authorization";
 
         // when
         jwtService.sendAccessToken(response, token);
@@ -93,12 +153,43 @@ public class JwtServiceTest {
         Assertions.assertThat(response.getHeader(accessTokenHeader)).isEqualTo(token);
     }
 
-    @DisplayName("getAccessTokenFromHeader : HttpServletRequest에서 token 가져오기 성공")
+    @DisplayName("sendRefreshToken : Header에 refreshToken 전송 성공")
+    @Test
+    public void sendRefreshTokenSuccess() throws Exception {
+        // given
+        HttpServletResponse response = new MockHttpServletResponse();
+        String token = "Test Token";
+
+        // when
+        jwtService.sendRefreshToken(response, token);
+
+        // then
+        Assertions.assertThat(response.getStatus()).isEqualTo(MockHttpServletResponse.SC_OK);
+        Assertions.assertThat(response.getHeader(refreshTokenHeader)).isEqualTo(token);
+    }
+
+    @DisplayName("sendAccessTokenAndRefreshToken : accessToken, refreshToken Header로 전송 성공")
+    @Test
+    public void sendAccessTokenAndRefreshToken() throws Exception {
+        // given
+        HttpServletResponse response = new MockHttpServletResponse();
+        String accessToken = "Test AccessToken";
+        String refreshToken = "Test RefreshToken";
+
+        // when
+        jwtService.sendAccessTokenAndRefreshToken(response, accessToken, refreshToken);
+
+        // then
+        Assertions.assertThat(response.getStatus()).isEqualTo(MockHttpServletResponse.SC_OK);
+        Assertions.assertThat(response.getHeader(accessTokenHeader)).isEqualTo(accessToken);
+        Assertions.assertThat(response.getHeader(refreshTokenHeader)).isEqualTo(refreshToken);
+    }
+
+    @DisplayName("getAccessTokenFromHeader : HttpServletRequest에서 accesstoken 가져오기 성공")
     @Test
     public void getAccessTokenFromHeaderSuccess() throws Exception {
         // given
         MockHttpServletRequest request = new MockHttpServletRequest();
-        String accessTokenHeader = "Authorization";
         String jwt_token = jwtService.createAccessToken("testEmail@email.com",Role.USER);
         String jwt_token_value = jwt_token.replace(BEARER_PREFIX, "");
 
@@ -110,6 +201,24 @@ public class JwtServiceTest {
         // then
         Assertions.assertThat(token).isNotEmpty();
         Assertions.assertThat(token.get()).isEqualTo(jwt_token_value);
+    }
+
+    @DisplayName("getRefreshTokenFromHeader : HttpServletRequest에서 refreshToken 가져오기 성공")
+    @Test
+    public void getRefreshTokenFromHeaderSuccess() throws Exception {
+        // given
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        String rjwt_token = jwtService.createRefreshToken("testEmail@email.com");
+        String rjwt_token_value = rjwt_token.replace(BEARER_PREFIX, "");
+
+        request.addHeader(refreshTokenHeader, rjwt_token);
+
+        // when
+        Optional<String> token = jwtService.getRefreshTokenFromHeader(request);
+
+        // then
+        Assertions.assertThat(token).isNotEmpty();
+        Assertions.assertThat(token.get()).isEqualTo(rjwt_token_value);
     }
 
     @DisplayName("validateToken : jwt token 검증 성공")
@@ -137,5 +246,90 @@ public class JwtServiceTest {
 
         // then
         Assertions.assertThat(email.get()).isEqualTo(origin_email);
+    }
+
+    @DisplayName("updateRefreshToken : refreshToken 업데이트 성공")
+    @Test
+    public void updateRefreshToken() throws Exception {
+        // given
+        String email = "testEmail@email.com";
+        String refreshToken = "test RefreshToken";
+
+        // when
+        when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+        doNothing().when(valueOperations).set(any(String.class), any(String.class));
+
+        jwtService.updateRefreshToken(email, refreshToken);
+
+        // then
+        verify(valueOperations,times(1)).set(any(String.class), any(String.class));
+    }
+
+    @DisplayName("getRefreshTokenFromRedisThroughEmail : redis DB에서 Email에 해당하는 value(refreshToken)값 가져오기 성공")
+    @Test
+    public void getRefreshTokenFromRedisThroughEmail() throws Exception {
+        // given
+        String email = "testEmail@email.com";
+        String refreshToken = "refreshToken";
+
+        // when
+        when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+        doReturn(refreshToken).when(valueOperations).get(anyString());
+
+        String result = jwtService.getRefreshTokenFromRedisThroughEmail(email).get();
+
+        // then
+        Assertions.assertThat(result).isEqualTo(refreshToken);
+    }
+
+    @DisplayName("deleteRefreshTokenByEmail : redis Email에 해당하는 데이터 삭제 성공")
+    @Test
+    public void deleteRefreshTokenByEmail() throws Exception {
+        // given
+        String email = "testEmail@email.com";
+
+        // when
+        doReturn(true).when(redisTemplate).delete(anyString());
+
+        jwtService.deleteRefreshTokenByEmail(email);
+
+        // then
+        verify(redisTemplate,times(1)).delete(anyString());
+    }
+
+    @DisplayName("deleteRefreshTokenByRefreshToken : redis refreshToken에서 email추출하여 해당하는 데이터 삭제 성공 ")
+    @Test
+    public void deleteRefreshTokenByRefreshTokenSuccess() throws Exception {
+        // given
+        String email = "testEmail@email.com";
+        String refreshToken = jwtService.createRefreshToken(email).split(" ")[1];
+
+        // when
+        doReturn(true).when(redisTemplate).delete(email);
+
+        jwtService.deleteRefreshTokenByRefreshToken(refreshToken);
+
+        // then
+        verify(redisTemplate,times(1)).delete(anyString());
+    }
+
+    @DisplayName("isEqualsRefreshToken : Header의 RefreshToken과 Redis의 RefreshToken이 동일한지 비교 성공")
+    @Test
+    public void isEqualsRefreshToken() throws Exception {
+        // given
+        String email = "testEmail@email.com";
+        String refreshToken = jwtService.createRefreshToken(email).split(" ")[1];
+
+        when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+        given(valueOperations.get(anyString())).willReturn(refreshToken);
+
+        // when
+        String curRefreshToken = jwtService.getRefreshTokenFromRedisThroughEmail(email).get();
+
+        boolean check = jwtService.isEqualsRefreshToken(refreshToken);
+
+        // then
+        Assertions.assertThat(check).isEqualTo(true);
+        Assertions.assertThat(curRefreshToken).isEqualTo(refreshToken);
     }
 }
