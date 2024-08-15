5cf44272a11183f8d29227d57f13c99b6977c2e1
ImJcm <wjsckdals0909@naver.com>
2024-08-15 21:36:31 +0900

    JwtController - RefreshToken 재발급 시, 호출 API 추가

--- src/main/java/com/imjcm/oauth2andloginpractice/domain/jwt/JwtController.java
+++ src/main/java/com/imjcm/oauth2andloginpractice/domain/jwt/JwtController.java
@@ -2,10 +2,9 @@ package com.imjcm.oauth2andloginpractice.domain.jwt;
 
 import com.imjcm.oauth2andloginpractice.global.common.Role;
 import com.imjcm.oauth2andloginpractice.global.config.jwt.service.JwtService;
+import com.nimbusds.oauth2.sdk.token.AccessToken;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +18,15 @@ public class JwtController {
         Role role = Role.USER;
 
         return jwtService.createAccessToken(email, role);
+    }
+
+    @GetMapping("/reissue-token")
+    public void reIssueRefreshToken() {
+        /*
+            AccessToken 유효성 검사 또는 만료 시, 예외발생하면 클라이언트에서 Header에 RefreshToken을 담아서
+            API 호출 시, JwtAuthenticationFilter에서 재발급 여부를 검사하고 재발급 수행한다.
+
+            따라서, 해당 API는 Jwt 재발급 호출용이다.
+         */
     }
 }
