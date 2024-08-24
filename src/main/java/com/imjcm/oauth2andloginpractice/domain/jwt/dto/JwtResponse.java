425ce0588ad7a1257b9698921bffe27e35551aaa
ImJcm <wjsckdals0909@naver.com>
2024-08-25 03:09:31 +0900

    JwtRequest - 재발급한 AccessToken을 Json Body에 담아서 전달하기 위한 ResponseDto (Not Used)

---
+++ src/main/java/com/imjcm/oauth2andloginpractice/domain/jwt/dto/JwtResponse.java
@@ -0,0 +1,12 @@
+/*
+package com.imjcm.oauth2andloginpractice.domain.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class JwtResponse {
+    private String accessToken;
+}
+*/
