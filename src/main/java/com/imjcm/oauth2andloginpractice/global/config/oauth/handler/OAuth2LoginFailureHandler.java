9cf8674faee7f1f73fba1a4420714d5198c5ecf9
ImJcm <wjsckdals0909@naver.com>
2024-08-25 03:19:50 +0900

    GoogleOAuth2MemberInfo - OAuth2MemberInfo를 상속받아 OAuth의 공통 데이터를 갖는 데이터 클래스 (Oauth 마다 AttributeName이 다르기 때문에 공통된 양식 구조가 필요하다)

---
+++ src/main/java/com/imjcm/oauth2andloginpractice/global/config/oauth/handler/OAuth2LoginFailureHandler.java
@@ -0,0 +1,25 @@
+package com.imjcm.oauth2andloginpractice.global.config.oauth.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.getWriter().write("소셜 로그인 실패.");
+        response.sendRedirect("/home/login");
+        log.info("소셜 로그인 실패. 에러 메시지 : {}", exception.getMessage());
+    }
+}
