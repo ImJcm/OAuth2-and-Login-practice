e1e87106dda71360821e98c89ae9ddc6a34c35b3
ImJcm <wjsckdals0909@naver.com>
2024-08-25 03:16:58 +0900

    OAuth2Attribute - OAuth에서 사용자 데이터를 저장하는 Map<String, Object>와 식별 PK 값을 필드로 갖는 데이터 클래스

---
+++ src/main/java/com/imjcm/oauth2andloginpractice/global/config/oauth/OAuth2Attribute.java
@@ -0,0 +1,52 @@
+package com.imjcm.oauth2andloginpractice.global.config.oauth;
+
+import com.imjcm.oauth2andloginpractice.domain.member.Member;
+import com.imjcm.oauth2andloginpractice.global.common.PasswordUtil;
+import com.imjcm.oauth2andloginpractice.global.common.Role;
+import com.imjcm.oauth2andloginpractice.global.common.SocialType;
+import com.imjcm.oauth2andloginpractice.global.config.oauth.info.GoogleOAuth2MemberInfo;
+import com.imjcm.oauth2andloginpractice.global.config.oauth.info.OAuth2MemberInfo;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuth2Attribute {
+    private String nameAttributeKey;
+    private OAuth2MemberInfo oAuth2MemberInfo;
+
+    @Builder
+    public OAuth2Attribute(String nameAttributeKey, OAuth2MemberInfo oAuth2MemberInfo) {
+        this.nameAttributeKey = nameAttributeKey;
+        this.oAuth2MemberInfo = oAuth2MemberInfo;
+    }
+
+    public static OAuth2Attribute of(SocialType socialType, String nameAttributeKey, Map<String, Object> attribute) {
+        return switch (socialType) {
+            case KAKAO -> null; //ofKakao(nameAttributeKey, attribute);
+            case NAVER -> null; //ofNaver(nameAttributeKey, attribute);
+            case GOOGLE -> ofGoogle(nameAttributeKey, attribute);
+        };
+    }
+
+    private static OAuth2Attribute ofGoogle(String nameAttributeKey, Map<String, Object> attribute) {
+        return OAuth2Attribute.builder()
+                .nameAttributeKey(nameAttributeKey)
+                .oAuth2MemberInfo(new GoogleOAuth2MemberInfo(attribute))
+                .build();
+    }
+
+    public Member toEntity(OAuth2MemberInfo oAuth2MemberInfo, SocialType socialType) {
+        return Member.builder()
+                .socialType(socialType)
+                .oauthId(oAuth2MemberInfo.getId())
+                .role(Role.USER)
+                .email(oAuth2MemberInfo.getEmail())
+                .password(PasswordUtil.generateRandomPassword())
+                .nickname(oAuth2MemberInfo.getNickname())
+                .oauthId(oAuth2MemberInfo.getId())
+                .build();
+    }
+
+}
