5b00ff8584c0feb52ef730977b0656b09fa6419c
ImJcm <wjsckdals0909@naver.com>
2024-08-25 03:15:54 +0900

    MemberRepository - socialType + oauthId가 일치하는 계정 찾는 Query Method 추가

--- src/main/java/com/imjcm/oauth2andloginpractice/domain/member/MemberRepository.java
+++ src/main/java/com/imjcm/oauth2andloginpractice/domain/member/MemberRepository.java
@@ -1,12 +1,16 @@
 package com.imjcm.oauth2andloginpractice.domain.member;
 
+import com.imjcm.oauth2andloginpractice.global.common.SocialType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
     boolean existsByEmail(String email);
+
+    Optional<Member> findBySocialTypeAndOauthId(SocialType socialType, String oAuthId);
 }
