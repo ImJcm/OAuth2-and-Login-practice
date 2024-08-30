3403088a4ce77d2c61a47d6b18bcee9b13245dfd
ImJcm <wjsckdals0909@naver.com>
2024-08-31 01:41:23 +0900

    LoginSuccessHandlerTest - onAuthenticationSuccessTest 메서드 수정

--- src/test/java/com/imjcm/oauth2andloginpractice/login/UnitTest/LoginSuccessHandlerTest.java
+++ src/test/java/com/imjcm/oauth2andloginpractice/login/UnitTest/LoginSuccessHandlerTest.java
@@ -1,10 +1,10 @@
 package com.imjcm.oauth2andloginpractice.login.UnitTest;
 
 import com.imjcm.oauth2andloginpractice.domain.member.Member;
-import com.imjcm.oauth2andloginpractice.domain.member.MemberRepository;
 import com.imjcm.oauth2andloginpractice.global.common.Role;
 import com.imjcm.oauth2andloginpractice.global.config.jwt.service.JwtService;
 import com.imjcm.oauth2andloginpractice.global.config.login.handler.LoginSuccessHandler;
+import jakarta.servlet.http.Cookie;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -20,6 +20,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
@@ -35,7 +38,8 @@ public class LoginSuccessHandlerTest {
 
     @BeforeEach
     void init() {
-        ReflectionTestUtils.setField(jwtService, "accessTokenExpirationsPeriod",1800000L);
+        ReflectionTestUtils.setField(jwtService, "accessTokenExpirationsPeriod",1800000);
+        ReflectionTestUtils.setField(jwtService, "refreshTokenExpirationsPeriod", 3600000);
     }
 
     @DisplayName("onAuthenticationSuccess : Authentication 성공 시 수행")
@@ -43,6 +47,7 @@ public class LoginSuccessHandlerTest {
     public void onAuthenticationSuccessTest() throws Exception {
         // given
         String jwt_Header = "Authorization";
+        String rjwt_Header = "Refresh_Authorization";
         String jwt_prefix = "Bearer ";
         String email = "testMember@email.com";
         String password = "testPassword";
@@ -60,23 +65,38 @@ public class LoginSuccessHandlerTest {
                 List.of(new SimpleGrantedAuthority(role.getAuthority())) // authorities
         );
 
-        String token = "jwt-token";
+        String accessToken = "jwt-token";
+        String refreshToken = "rjwt-token";
 
         given(jwtService.createAccessToken(email))
-                .willReturn(token);
+                .willReturn(accessToken);
+
+        given(jwtService.createRefreshToken(email))
+                .willReturn(refreshToken);
 
         doAnswer(invocationOnMock -> {
-            response.addHeader(jwt_Header, jwt_prefix + token);
+            response.addHeader(jwt_Header, jwt_prefix + accessToken);
+            response.addCookie(new Cookie(rjwt_Header, jwt_prefix + refreshToken));
             return null;
-        }).when(jwtService).sendAccessTokenByHeader(eq(response),eq(token));
+        }).when(jwtService).sendAccessTokenAndRefreshToken(eq(request),eq(response),eq(accessToken),eq(refreshToken));
 
         // when
         loginSuccessHandler.onAuthenticationSuccess(request, response, authentication);
 
+        String r_value = Arrays.stream(response.getCookies())
+                .filter(cookie -> cookie.getName().equals(rjwt_Header))
+                .findFirst()
+                .map(Cookie::getValue)
+                .map(v -> URLDecoder.decode(v, StandardCharsets.UTF_8).replace(jwt_prefix,""))
+                .orElse(null);
+
         // then
         verify(jwtService).createAccessToken(eq(email));
-        verify(jwtService).sendAccessTokenByHeader(eq(response), eq(token));
+        verify(jwtService).createRefreshToken(eq(email));
+        verify(jwtService).updateRefreshToken(eq(email), eq(refreshToken));
+        verify(jwtService).sendAccessTokenAndRefreshToken(eq(request), eq(response), eq(accessToken), eq(refreshToken));
 
-        Assertions.assertThat(response.getHeader(jwt_Header)).isEqualTo(jwt_prefix + token);
+        Assertions.assertThat(response.getHeader(jwt_Header)).isEqualTo(jwt_prefix + accessToken);
+        Assertions.assertThat(r_value).isEqualTo(refreshToken);
     }
 }
